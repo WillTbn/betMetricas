1d612b95d6437db4e575e30d6bab182d4a903efd
JorgeNunes <jlbnunes@live.com>
2023-08-19 02:43:54 -0300

    feature: add paramns with filters field date refactor: terms cookie

--- src/store/essencial/actions.js
+++ src/store/essencial/actions.js
@@ -1,7 +1,1 @@
-export function updateStatusCookie(context, status) {
-    // if (status === true) {
-    //     document.cookie = "terms=false";
-    // }
-    console.log(status);
-    context.commit("setStatusCookie", true);
-}
+
